e9474182eb0e921d0c39fa25b04bd29e0f64a778
suyong <emuext@gmail.com>
2023-08-08 22:43:37 +0900

    User Microservice와 API Gateway 연동, 사용자 조회기능 추가

--- src/main/java/com/example/userservice/jpa/UserRepository.java
+++ src/main/java/com/example/userservice/jpa/UserRepository.java
@@ -2,7 +2,9 @@ package com.example.userservice.jpa;
 
 import org.springframework.data.repository.CrudRepository;
 
-public interface UserRepository extends CrudRepository<UserEntity, Long> { //데이터베이스 Entity 클래스, 기본키의 클래스
+public interface UserRepository extends CrudRepository<UserEntity, Long> {
+
+    UserEntity findByUserId(String userId); //데이터베이스 Entity 클래스, 기본키의 클래스
 
 
 }
